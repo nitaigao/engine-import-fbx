f2cf07ed220de1be883b8153690850a5d4f8dddb
nkostelnik <nkostelnik@gmail.com>
2011-07-28 23:27:02 +0100

    Initial Commit

---
+++ fbx/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  fbx
+//
+//  Created by Nicholas Kostelnik on 28/07/2011.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#include <iostream>
+
+int main (int argc, const char * argv[])
+{
+
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
+
