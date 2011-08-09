1ecfe0457fd4c0764e0f764a0157027bcbbe9d14
Nick Kostelnik <nkostelnik@gmail.com>
2011-08-09 23:42:39 +0100

    uv's just about working

--- fbx/fbx_json_serializer.cpp
+++ fbx/fbx_json_serializer.cpp
@@ -27,46 +27,247 @@ void FBXJSONSerializer::serialize(const char *input_filename, const char *output
   importer->Destroy();
 
   KFbxNode* root_node = scene->GetRootNode();
-  Object json_root;
-  
-  Array meshes;  
-  this->recurse_over_model(root_node, meshes);
+  KFbxGeometryConverter converter(root_node->GetFbxSdkManager());
+    Object json_root;
+    
+    Array meshes;  
+    this->recurse_over_model(root_node, meshes);
 
-  json_root["meshes"] = meshes;
+    json_root["meshes"] = meshes;
 
-  std::ofstream stream(output_filename);
-  Writer::Write(json_root, stream);
+    std::ofstream stream(output_filename);
+    Writer::Write(json_root, stream);
 }
 
+void LoadUVInformation(KFbxMesh* pMesh, Array& json_mesh_uvs)
+{
+  //get all UV set names
+  KStringList lUVSetNameList;
+  pMesh->GetUVSetNames(lUVSetNameList);
+  
+  //iterating over all uv sets
+  for (int lUVSetIndex = 0; lUVSetIndex < lUVSetNameList.GetCount(); lUVSetIndex++)
+  {
+    //get lUVSetIndex-th uv set
+    const char* lUVSetName = lUVSetNameList.GetStringAt(lUVSetIndex);
+    const KFbxGeometryElementUV* lUVElement = pMesh->GetElementUV(lUVSetName);
+    
+    if(!lUVElement)
+      continue;
+    
+    // only support mapping mode eBY_POLYGON_VERTEX and eBY_CONTROL_POINT
+    if( lUVElement->GetMappingMode() != KFbxGeometryElement::eBY_POLYGON_VERTEX &&
+       lUVElement->GetMappingMode() != KFbxGeometryElement::eBY_CONTROL_POINT )
+      return;
+    
+    //    //direct array, where holds the actual uv data
+    //    const int lDataCount = lUVElement->GetDirectArray().GetCount();
+    
+    //index array, where holds the index referenced to the uv data
+    const bool lUseIndex = lUVElement->GetReferenceMode() != KFbxGeometryElement::eDIRECT;
+    const int lIndexCount= (lUseIndex) ? lUVElement->GetIndexArray().GetCount() : 0;
+    
+    //iterating through the data by polygon
+    const int lPolyCount = pMesh->GetPolygonCount();
+    
+    if( lUVElement->GetMappingMode() == KFbxGeometryElement::eBY_CONTROL_POINT )
+    {
+      for( int lPolyIndex = 0; lPolyIndex < lPolyCount; ++lPolyIndex )
+      {
+        // build the max index array that we need to pass into MakePoly
+        const int lPolySize = pMesh->GetPolygonSize(lPolyIndex);
+        for( int lVertIndex = 0; lVertIndex < lPolySize; ++lVertIndex )
+        {
+          KFbxVector2 lUVValue;
+          
+          //get the index of the current vertex in control points array
+          int lPolyVertIndex = pMesh->GetPolygonVertex(lPolyIndex,lVertIndex);
+          
+          //the UV index depends on the reference mode
+          int lUVIndex = lUseIndex ? lUVElement->GetIndexArray().GetAt(lPolyVertIndex) : lPolyVertIndex;
+          
+          lUVValue = lUVElement->GetDirectArray().GetAt(lUVIndex);
+          
+          //User TODO:
+          //Print out the value of UV(lUVValue) or log it to a file
+          
+          Number uv_x = lUVValue.GetAt(0); json_mesh_uvs.Insert(uv_x);
+          Number uv_y = lUVValue.GetAt(1); json_mesh_uvs.Insert(uv_y);
+          
+        }
+      }
+    }
+    else if (lUVElement->GetMappingMode() == KFbxGeometryElement::eBY_POLYGON_VERTEX)
+    {
+      int lPolyIndexCounter = 0;
+      for( int lPolyIndex = 0; lPolyIndex < lPolyCount; ++lPolyIndex )
+      {
+        // build the max index array that we need to pass into MakePoly
+        const int lPolySize = pMesh->GetPolygonSize(lPolyIndex);
+        for( int lVertIndex = 0; lVertIndex < lPolySize; ++lVertIndex )
+        {
+          if (lPolyIndexCounter < lIndexCount)
+          {
+            KFbxVector2 lUVValue;
+            
+            //the UV index depends on the reference mode
+            int lUVIndex = lUseIndex ? lUVElement->GetIndexArray().GetAt(lPolyIndexCounter) : lPolyIndexCounter;
+            
+            lUVValue = lUVElement->GetDirectArray().GetAt(lUVIndex);
+            
+            //User TODO:
+            //Print out the value of UV(lUVValue) or log it to a file
+            
+            Number uv_x = lUVValue.GetAt(0); json_mesh_uvs.Insert(uv_x);
+            Number uv_y = lUVValue.GetAt(1); json_mesh_uvs.Insert(uv_y);
+            
+            lPolyIndexCounter++;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+
+
+void LoadNormalInformation(KFbxMesh* lMesh, Array& json_mesh_normals) {
+  //get the normal element
+  KFbxGeometryElementNormal* lNormalElement = lMesh->GetElementNormal();
+  if(lNormalElement)
+  {
+    //mapping mode is by control points. The mesh should be smooth and soft.
+    //we can get normals by retrieving each control point
+    if( lNormalElement->GetMappingMode() == KFbxGeometryElement::eBY_CONTROL_POINT )
+    {
+      //Let's get normals of each vertex, since the mapping mode of normal element is by control point
+      for(int lVertexIndex = 0; lVertexIndex < lMesh->GetControlPointsCount(); lVertexIndex++)
+      {
+        int lNormalIndex = 0;
+        //reference mode is direct, the normal index is same as vertex index.
+        //get normals by the index of control vertex
+        if( lNormalElement->GetReferenceMode() == KFbxGeometryElement::eDIRECT )
+          lNormalIndex = lVertexIndex;
+        
+        //reference mode is index-to-direct, get normals by the index-to-direct
+        if(lNormalElement->GetReferenceMode() == KFbxGeometryElement::eINDEX_TO_DIRECT)
+          lNormalIndex = lNormalElement->GetIndexArray().GetAt(lVertexIndex);
+        
+        //Got normals of each vertex.
+        KFbxVector4 lNormal = lNormalElement->GetDirectArray().GetAt(lNormalIndex);
+        printf("normals for vertex[%d]: %f %f %f %f \n", lVertexIndex, lNormal[0], lNormal[1], lNormal[2], lNormal[3]);
+        //add your custom code here, to output normals or get them into a list, such as KArrayTemplate<KFbxVector4>
+        //. . .
+        //Got normals of each vertex.
+//        KFbxVector4 lNormal = lNormalElement->GetDirectArray().GetAt(lNormalIndex);          
+        Number normal_x = lNormal.GetAt(0); json_mesh_normals.Insert(normal_x);
+        Number normal_y = lNormal.GetAt(1); json_mesh_normals.Insert(normal_y);
+        Number normal_z = lNormal.GetAt(2); json_mesh_normals.Insert(normal_z);          
+
+      }//end for lVertexIndex
+    }//end eBY_CONTROL_POINT
+    //mapping mode is by polygon-vertex.
+    //we can get normals by retrieving polygon-vertex.
+    else if(lNormalElement->GetMappingMode() == KFbxGeometryElement::eBY_POLYGON_VERTEX)
+    {
+      int lIndexByPolygonVertex = 0;
+      //Let's get normals of each polygon, since the mapping mode of normal element is by polygon-vertex.
+      for(int lPolygonIndex = 0; lPolygonIndex < lMesh->GetPolygonCount(); lPolygonIndex++)
+      {
+        //get polygon size, you know how many vertices in current polygon.
+        int lPolygonSize = lMesh->GetPolygonSize(lPolygonIndex);
+        //retrieve each vertex of current polygon.
+        for(int i = 0; i < lPolygonSize; i++)
+        {
+          int lNormalIndex = 0;
+          //reference mode is direct, the normal index is same as lIndexByPolygonVertex.
+          if( lNormalElement->GetReferenceMode() == KFbxGeometryElement::eDIRECT )
+            lNormalIndex = lIndexByPolygonVertex;
+          
+          //reference mode is index-to-direct, get normals by the index-to-direct
+          if(lNormalElement->GetReferenceMode() == KFbxGeometryElement::eINDEX_TO_DIRECT)
+            lNormalIndex = lNormalElement->GetIndexArray().GetAt(lIndexByPolygonVertex);
+          
+          //Got normals of each polygon-vertex.
+          KFbxVector4 lNormal = lNormalElement->GetDirectArray().GetAt(lNormalIndex);
+          printf("normals for polygon[%d]vertex[%d]: %f %f %f %f \n", 
+                 lPolygonIndex, i, lNormal[0], lNormal[1], lNormal[2], lNormal[3]);
+          //add your custom code here, to output normals or get them into a list, such as KArrayTemplate<KFbxVector4>
+          //. . .
+          
+          //Got normals of each vertex.
+//          KFbxVector4 lNormal = lNormalElement->GetDirectArray().GetAt(lNormalIndex);          
+          Number normal_x = lNormal.GetAt(0); json_mesh_normals.Insert(normal_x);
+          Number normal_y = lNormal.GetAt(1); json_mesh_normals.Insert(normal_y);
+          Number normal_z = lNormal.GetAt(2); json_mesh_normals.Insert(normal_z);          
+
+          
+          lIndexByPolygonVertex++;
+        }//end for i //lPolygonSize
+      }//end for lPolygonIndex //PolygonCount
+      
+    }//end eBY_POLYGON_VERTEX
+  }//end if lNormalElement
+}
+
+
 void FBXJSONSerializer::recurse_over_model(fbxsdk_2012_1::KFbxNode *fbx_node, json::Array& meshes_array) {
-  KFbxGeometryConverter converter(fbx_node->GetFbxSdkManager());
-    
-  if (converter.TriangulateInPlace(fbx_node)) {
-    KFbxMesh* mesh = fbx_node->GetMesh();
-    
-    if (mesh) {
+//  KFbxGeometryConverter converter(fbx_node->GetFbxSdkManager());
+//  if (converter.TriangulateInPlace(fbx_node)) {
+    KFbxMesh* old_mesh = fbx_node->GetMesh();
+    if (old_mesh) {
+//      KFbxMesh* mesh = old_mesh;
+      KFbxGeometryConverter converter(fbx_node->GetFbxSdkManager());
+      KFbxMesh* mesh = converter.TriangulateMesh(old_mesh);
+      mesh->ComputeBBox();
+      mesh->ComputeVertexNormals();
+      
       Object json_mesh;
       {
+//        LoadNormalInformation(mesh, json_mesh_normals);
+      }
+      { 
+//        Array json_mesh_uvs;
+//        LoadUVInformation(mesh, json_mesh_uvs);
+//        json_mesh["uvs"] = json_mesh_uvs;
+      }
+      {
+        KFbxLayerElementUV* uvs = mesh->GetLayer(0)->GetUVs();
+        KFbxLayerElementNormal* normals = mesh->GetLayer(0, KFbxLayerElement::eNORMAL)->GetNormals();
+        Array json_mesh_normals;        
+        Array json_mesh_uvs;
         Array json_mesh_vertices;
-        Array json_mesh_normals;
-        KFbxGeometryElementNormal* normalElement = mesh->GetElementNormal();
-        for (int poly_index = 0; poly_index < mesh->GetPolygonCount(); poly_index++) {
-          for (int vertex_index = 0; vertex_index < 3; vertex_index++) {
+        int polygon_count = mesh->GetPolygonCount();
+        for (int poly_index = 0; poly_index < polygon_count; poly_index++) {
+          std::clog << mesh->GetPolygonSize(poly_index) << std::endl;
+          for (int vertex_index = 0; vertex_index < mesh->GetPolygonSize(poly_index); vertex_index++) {
             int vertex_position = mesh->GetPolygonVertex(poly_index, vertex_index);
             
             KFbxVector4 vertex = mesh->GetControlPoints()[vertex_position];
             Number vertex_x = vertex.GetAt(0); json_mesh_vertices.Insert(vertex_x);
             Number vertex_y = vertex.GetAt(1); json_mesh_vertices.Insert(vertex_y);
             Number vertex_z = vertex.GetAt(2); json_mesh_vertices.Insert(vertex_z);
-            
-            KFbxVector4 normal = normalElement->GetDirectArray()[vertex_position];
+
+            KFbxVector4 normal = normals->GetDirectArray()[vertex_position];
+//            mesh->GetPolygonVertexNormal(poly_index, vertex_index, normal);
+//
             Number normal_x = normal.GetAt(0); json_mesh_normals.Insert(normal_x);
             Number normal_y = normal.GetAt(1); json_mesh_normals.Insert(normal_y);
-            Number normal_z = normal.GetAt(2); json_mesh_normals.Insert(normal_z);
+            Number normal_z = normal.GetAt(2); json_mesh_normals.Insert(normal_z); 
+
+            
+            int mesh_index = mesh->GetTextureUVIndex(poly_index, vertex_index);
+            KFbxVector2 uv = uvs->GetDirectArray().GetAt(mesh_index);
+            
+            Number uv_x = 1.0f-uv[0]; json_mesh_uvs.Insert(uv_x); // these are flipped
+            Number uv_y = 1.0f -uv[1]; json_mesh_uvs.Insert(uv_y);
           }
         }
         json_mesh["vertices"] = json_mesh_vertices;
         json_mesh["normals"] = json_mesh_normals;
+        json_mesh["uvs"] = json_mesh_uvs;
+
       }
       {
         fbxDouble3 scale = fbx_node->LclScaling.Get();
@@ -158,13 +359,13 @@ void FBXJSONSerializer::recurse_over_model(fbxsdk_2012_1::KFbxNode *fbx_node, js
         }
         json_mesh["materials"] = json_mesh_materials;
       }
+      json_mesh["uv_stride"] = Number(2);
       json_mesh["vertex_stride"] = Number(3);
       json_mesh["normal_stride"] = Number(3);
       meshes_array.Insert(json_mesh);
     }
     
-  }
-  
+//  }
   for(int j = 0; j < fbx_node->GetChildCount(); j++) {
     recurse_over_model(fbx_node->GetChild(j), meshes_array);
   }
